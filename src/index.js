3de554b7f7ed4efb16ecaeee8e5950e4a7abdf41
Taeil Kwak <taeil2@gmail.com>
2020-05-11 10:50:50 -0500

    setting up the project

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+import store from './store';
+
+ReactDOM.render(<App store={store} />, document.getElementById('root'));
